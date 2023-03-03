d45b1bf50f66f11ec0435da665dba629b7ecdac2
Ademir Giraldelli <16231827+aeagiraldelli@users.noreply.github.com>
2023-03-03 20:45:33 -0300

    finished chapter 05

---
+++ 05-type-conversion/index.js
@@ -0,0 +1,25 @@
+// let age = window.prompt("How old are you?");
+// console.log(typeof(age));
+// age = Number(age) + 1;
+// console.log(typeof(age));
+// console.log("Happy birthday! You are", age, "years old.");
+
+let x;
+let y;
+let z;
+let a;
+
+x = Number("3.14");
+console.log("x = ", x, typeof x);
+
+y = String(3.14);
+console.log("y = ", y, typeof y);
+
+z = Boolean("")
+console.log("z = ", z, typeof z);
+
+z = Boolean("pizza");
+console.log("z = ", z, typeof z);
+
+a = Number("pizza");
+console.log("a = ", a, typeof a);
