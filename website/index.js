6fcfa300c8aec1050c1af777dd62d3a66ac51c6b
Ademir Giraldelli <16231827+aeagiraldelli@users.noreply.github.com>
2023-03-02 21:25:38 -0300

    first commit

---
+++ website/index.js
@@ -0,0 +1,22 @@
+console.log("I like pizza!");
+console.log("I'ts really good!");
+
+// This is a comment.
+
+/*
+    This
+    is
+    a 
+    multiline
+    comment.
+*/
+
+// ------------------- VARIABLES -------------------
+
+let firstName = "Ademir";
+let age = 35;
+let isStudent = false;
+
+document.getElementById("p1").innerHTML = "Hello, " + firstName;
+document.getElementById("p2").innerHTML = "You are " + age + " years old.";
+document.getElementById("p3").innerHTML = "Enrolled: " + isStudent;
