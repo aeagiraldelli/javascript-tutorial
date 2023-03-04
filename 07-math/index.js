a5a21f548d491d30d3e90921442018e040835ded
Ademir Giraldelli <16231827+aeagiraldelli@users.noreply.github.com>
2023-03-03 21:05:51 -0300

    finished chapter 07.

---
+++ 07-math/index.js
@@ -0,0 +1,22 @@
+let x = 3.14;
+
+// x = Math.round(x);
+// console.log(x);
+// x = Math.floor(x);
+// console.log(x);
+// x = Math.ceil(x);
+// console.log(x);
+// x = Math.pow(x, 2);
+// console.log(x);
+// x = Math.sqrt(x);
+// console.log(x);
+// x = Math.abs(x);
+// console.log(x);
+console.log(Math.PI);
+console.log(Math.E);
+
+let result = Math.max(Math.PI, Math.E);
+console.log(result);
+
+result = Math.min(Math.PI, Math.E);
+console.log(result);
